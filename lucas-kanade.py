c42234c22954f474c39f78e442ea3b6f78bba041
Prakhar Jagwani <prakharjagwani7@gmail.com>
2021-10-18 22:48:14 +0530

    try lk

--- lucas-kanade.py
+++ lucas-kanade.py
@@ -1,6 +1,7 @@
 import cv2
 from matplotlib.path import Path
 import numpy as np
+import os
 
 def checkConverge(p,delta_p,e):
     a = np.array([[0,0,1],[1,0,1],[0,1,1],[1,1,1]])
@@ -29,6 +30,12 @@ def bilinearInterpolate(arr,coord):
     wc = (x-x0) * (y1-y)
     wd = (x-x0) * (y-y0)
 
+    if len(arr.shape) == 4:
+        wa = np.expand_dims(wa,(1,2))
+        wb = np.expand_dims(wb,(1,2))
+        wc = np.expand_dims(wc,(1,2))
+        wd = np.expand_dims(wd,(1,2))
+
     return wa*arr[y0,x0] + wb*arr[y1,x0] + wc*arr[y0,x1] + wd*arr[y1,x1]
 
 def warp(coord,p):
@@ -51,8 +58,8 @@ def getDiff(prev_frame,curr_frame,coord_p,p):
     # return        |T| X 1
 
     warped_coord = warp(coord_p,p)              # |T| X 2
-    diff = bilinearInterpolate(prev_frame,coord_p) - bilinearInterpolate(prev_frame,warped_coord) # |T| X 1
-    return diff
+    diff = bilinearInterpolate(prev_frame,coord_p) - bilinearInterpolate(curr_frame,warped_coord) # |T| X 1
+    return np.expand_dims(diff,-1)
 
 def getDeltaW(coord,a):
     # coord_n   |T| X 3
@@ -80,7 +87,8 @@ def getDeltaW(coord,a):
 def getCoords(bounding_box):
     x1,y1 = np.min(bounding_box,axis=0)
     x2,y2 = np.max(bounding_box,axis=0)
-    return np.array([[[i,j,1] for j in range(y1,y2+1)] for i in range(x1,x2+1)])
+    print(bounding_box)
+    return np.array([[[i,j,1] for j in range(y1,y2+1)] for i in range(x1,x2+1)]).reshape((-1,3))
 
 def getDeltaI(frame):
     # frame     m x n
@@ -97,16 +105,88 @@ def getDeltaP(prev_frame,curr_frame,coord_p,p):
     # bounding_box      4 X 2
     # coord_p = getCoords(bounding_box)                           # |T| X 3
     coord_n = warp(coord_p,p)                                   # |T| X 2
+    # print("coord_n",coord_n.shape)
     deltaW = getDeltaW(coord_p,p)                               # |T| X 2 X 9
-    deltaI = bilinearInterpolate(getDeltaI(curr_frame,coord_n)) # |T| X 1 X 2
+    # print("deltaW",deltaW.shape)
+    deltaI = bilinearInterpolate(getDeltaI(curr_frame),coord_n) # |T| X 1 X 2
+    # print("deltaI",deltaI.shape)
     deltaI_W = deltaI @ deltaW                                  # |T| X 1 X 9
+    # print("deltaI_W",deltaI_W.shape)
     deltaI_W_T = np.transpose(deltaI_W,(0,2,1))                 # |T| X 9 X 1
+    # print("deltaI_W_T",deltaI_W_T.shape)
     H = np.sum(deltaI_W_T @ deltaI_W,axis=0)                    # 9 X 9
+    # print("H",H.shape)
     H_inv = np.linalg.inv(H)
+    # print("H_inv",H_inv.shape)
     diff = getDiff(prev_frame,curr_frame,coord_p,p)             # |T| X 1
+    # print("diff",diff.shape)
+    # print(np.sum(diff))
     deltaP = H_inv @ np.sum(deltaI_W_T[:,:,0] * diff,axis=0)    # 9
+    # print("deltaP",deltaP.shape)
 
     deltaP = np.reshape(deltaP,(3,3))
     # bounding_box_new = warp(np.concatenate(bounding_box,np.ones(1,4),axis=1),p)
 
     return deltaP
+
+def iterate(prev_frame,curr_frame,coord_p,p):
+    for i in range(20):
+        deltaP = getDeltaP(prev_frame,curr_frame,coord_p,p)
+        # print(deltaP)
+        # if checkConverge(p,deltaP,0.0001):
+        #     break
+        p+=deltaP
+        # print(np.reshape(p,(9,)))
+    print(np.around(p))
+    return p
+
+def drawBound(params,template_box,frame):
+    corner_points = [template_box[0], [template_box[0][0], template_box[1][1]], template_box[1], [template_box[1][0], template_box[0][1]]]
+    warped_corner_points = warp(np.concatenate([np.array(corner_points),np.ones([4,1])],axis=1),params).astype('int32')
+    warped_corner_points = warped_corner_points.reshape((-1, 1, 2))
+    # print(warped_corner_points,corner_points)
+    return cv2.polylines(frame.copy(), [warped_corner_points], True, (255, 0, 0), 2)
+
+def getRect(val):
+    val = val.replace("\t", ",")
+    bounds = val.rstrip().split(",")
+    bounds = [int(x) for x in bounds]
+    return bounds
+
+def LK_run():
+    frames = []
+    path_vid = "A2/BlurCar2/"
+    filenames = os.listdir(path_vid+'img/')
+    groundtruth_file = open(path_vid+'groundtruth_rect.txt')
+    groundtruth_rect = groundtruth_file.readlines()
+    groundtruth_rect = [getRect(x) for x in groundtruth_rect]
+    for filename in filenames:
+        frame = cv2.imread(os.path.join(path_vid+'img/', filename),0)
+        frames.append(frame)
+
+    frames = np.array(frames)
+    #Get initial template
+    template = frames[0][groundtruth_rect[0][1]:(groundtruth_rect[0][1]+groundtruth_rect[0][3]), groundtruth_rect[0][0]:(groundtruth_rect[0][0]+groundtruth_rect[0][2])].copy()
+    template_start_point = (groundtruth_rect[0][0], groundtruth_rect[0][1])
+    template_end_point = (groundtruth_rect[0][0]+groundtruth_rect[0][2], groundtruth_rect[0][1]+groundtruth_rect[0][3])
+    template_box = (template_start_point, template_end_point)
+    print(template_box)
+    coord = getCoords(np.array([template_start_point,[template_start_point[0],template_end_point[1]],[template_end_point[0],template_start_point[1]],template_end_point]))
+    print("coord",coord.shape,np.min(coord,axis=0),np.max(coord,axis=0))
+    p = np.eye(3)
+    for i in range(1, len(frames)):
+        frame = frames[i]
+        p = iterate(frames[0], frame, coord, p)
+        cv2.imshow("template tracking LK", drawBound(p,template_box,frame))
+        # template_track, template, template_start_point = blockBasedTracking(frame, template, template_start_point, cv2.TM_CCORR_NORMED)
+        # bounding_rect.append((template_start_point[0], template_start_point[1], template.shape[1], template.shape[0]))
+        # cv2.imshow("template tracking block based", template_track)
+        start_point = (groundtruth_rect[i][0], groundtruth_rect[i][1])
+        end_point = (groundtruth_rect[i][0]+groundtruth_rect[i][2], groundtruth_rect[i][1]+groundtruth_rect[i][3])
+        frame = cv2.rectangle(frame, start_point, end_point, (255, 0, 0), 2)
+        # cv2.imshow("template tracking groundtruth", frame)
+        k = cv2.waitKey(0)
+        if k==27:
+            break
+    # print("mIOU: "+str(getMeanIOUScore(bounding_rect, groundtruth_rect)))
+    cv2.destroyAllWindows()
