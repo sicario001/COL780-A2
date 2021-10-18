8c1cd660e6d166b98004ec0d272bf29dfc0a9122
Prakhar Jagwani <prakharjagwani7@gmail.com>
2021-10-18 10:50:48 +0530

    add delta p

--- lucas-kanade.py
+++ lucas-kanade.py
@@ -1,48 +1,79 @@
 import numpy as np
+import cv2
 
-def warp(m,n,p):
-    # p should be 3x3
+def warp(coord,p):
+    # coord_n   |T| X 3
+    # p         3 X 3
+    # return    |T| X 2
     
-    coord_n = np.array([[[i,j,1] for j in range(n)] for i in range(m)])
-    
-    d = np.dot(coord_n,p[2])
-    x = np.dot(coord_n,p[0])/d
-    y = np.dot(coord_n,p[1])/d
-    f = np.stack([x,y],axis=2)
+    d = np.dot(coord,p[2])
+    x = np.dot(coord,p[0])/d
+    y = np.dot(coord,p[1])/d
+    f = np.stack([x,y],axis=1)
     return np.round(f).astype('int32')
 
 
-def getDiff(prev_frame,curr_frame,t_mask,p):
-    # t_mask is list of coordinates in template, shape n X 2
-    warped_coord = warp(prev_frame.shape[0],prev_frame.shape[1],p)
-    # TODO: round off?
-    curr_t_mask = warped_coord[t_mask[:,0],t_mask[:,1]]
-    print(curr_t_mask)
-    diff = prev_frame[t_mask[:,0],t_mask[:,1]] - curr_frame[curr_t_mask[:,0],curr_t_mask[:,1]]
+def getDiff(prev_frame,curr_frame,coord_p,p):
+    # prev_frame    m X n
+    # curr_frame    m X n
+    # coord_p       |T| X 2
+    # p             3 X 3
+    # return        |T| X 1
+
+    warped_coord = warp(coord_p,p)              # |T| X 2
+    diff = prev_frame[coord_p[:,0],coord_p[:,1]] - curr_frame[warped_coord[:,0],warped_coord[:,1]] # |T| X 1
     return diff
 
-def getDelta(m,n,a):
-    coord_n = np.array([[[i,j,1] for j in range(n)] for i in range(m)])
-    # for x,y return 2d matrix 
-    # [
-    #   a11x+a12y+a13   a21x+a22y+a23
-    #   a31x+a32y+a33   a41x+a42y+a43
-    # ]
-    d0 = np.expand_dims(np.dot(coord_n,a[0]),axis=-1)
-    d1 = np.expand_dims(np.dot(coord_n,a[1]),axis=-1)
-    d2 = np.expand_dims(np.dot(coord_n,a[2]),axis=-1)
+def getDeltaW(coord,a):
+    # coord_n   |T| X 3
+    # a         3 X 3
+    # returns   |T| X 2 X 9
+    d0 = np.expand_dims(np.dot(coord,a[0]),axis=-1)
+    d1 = np.expand_dims(np.dot(coord,a[1]),axis=-1)
+    d2 = np.expand_dims(np.dot(coord,a[2]),axis=-1)
 
-    J0_012 = coord_n/d2
+    J0_012 = coord/d2
     J0_345 = np.zeros_like(J0_012)
-    J0_678 = -coord_n*d0/d2**2
+    J0_678 = -coord*d0/d2**2
 
     J1_012 = np.zeros_like(J0_012)
-    J1_345 = coord_n/d2
-    J1_678 = -coord_n*d1/d2**2
+    J1_345 = coord/d2
+    J1_678 = -coord*d1/d2**2
 
     # row 1
-    r1 = np.concatenate([J0_012,J0_345,J0_678],axis=2)
+    r1 = np.concatenate([J0_012,J0_345,J0_678],axis=1)
     # row 2
-    r2 = np.concatenate([J1_012,J1_345,J1_678],axis=2)
+    r2 = np.concatenate([J1_012,J1_345,J1_678],axis=1)
     # final
-    return np.stack([r1,r2],axis=2)
+    return np.stack([r1,r2],axis=1)
+
+def getCoords(bounding_box):
+    pass
+
+def getDeltaI(frame):
+    # frame     m x n
+    # return    m X n X 1 X 2
+
+    # TODO: Same shape as original?
+    Ix = cv2.Sobel(frame, cv2.CV_64F, 1, 0, ksize=5)
+    Iy = cv2.Sobel(frame, cv2.CV_64F, 0, 1, ksize=5)
+    Ix_n = np.asarray( Ix[:,:] )
+    Iy_n = np.asarray( Iy[:,:] )
+    return np.stack([Ix_n,Iy_n],axis=2)    
+
+def getDeltaP(prev_frame,curr_frame,bounding_box,p):
+    # bounding_box      4 X 2
+    coord_p = getCoords(bounding_box)                           # |T| X 3
+    coord_n = warp(coord_p,p)                                   # |T| X 2
+    deltaW = getDeltaW(coord_p,p)                               # |T| X 2 X 9
+    deltaI = getDeltaI(curr_frame)[coord_n[:,0],coord_n[:,1]]   # |T| X 1 X 2
+    deltaI_W = deltaI @ deltaW                                  # |T| X 1 X 9
+    deltaI_W_T = np.transpose(deltaI_W,(0,2,1))                 # |T| X 9 X 1
+    H = np.sum(deltaI_W_T @ deltaI_W,axis=0)                    # 9 X 9
+    H_inv = np.linalg.inv(H)
+    diff = getDiff(prev_frame,curr_frame,coord_p,p)             # |T| X 1
+    deltaP = H_inv @ np.sum(deltaI_W_T[:,:,0] * diff,axis=0)    # 9
+
+    bounding_box_new = warp(np.concatenate(bounding_box,np.ones(1,4),axis=1),p)
+
+    return deltaP,bounding_box_new
